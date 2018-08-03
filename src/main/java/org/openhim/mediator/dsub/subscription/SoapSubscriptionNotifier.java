a22b14a7cac60e9465dc4fba3dd1af8a5cfc60ea
Arkadiusz Lalo <alalo@soldevelo.com>
2018-08-03 11:59:23 +0200

    ILI-57: Added document id to the SOAP subscription notifier

--- src/main/java/org/openhim/mediator/dsub/subscription/SoapSubscriptionNotifier.java
+++ src/main/java/org/openhim/mediator/dsub/subscription/SoapSubscriptionNotifier.java
@@ -3,7 +3,7 @@ package org.openhim.mediator.dsub.subscription;
 public class SoapSubscriptionNotifier implements SubscriptionNotifier {
 
     @Override
-    public void notifySubscription(Subscription subscription) {
+    public void notifySubscription(Subscription subscription, String documentId) {
         // TODO: SOAP client call
     }
 }
